cf01b875bbdecbf1e151f408086a0b1fb93b59aa
SoloZambously Developpement <zambouslysolo@gmail.com>
2025-03-31 13:01:40 +0200

    fix: bug

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # MASTERMIND - Projet en NSI 2025
 ## Antoine & Solal
+[Lien Notion (Suivi du projet)](https://fir-hide-32d.notion.site/Commande-GIT-1c7f4791454b80f8b25bf8c1450155db?pvs=4)
--- main.py
+++ main.py
@@ -18,26 +18,30 @@ def correct_colors_wrong_position(random_color_sequence, guess):
     """
     Count the number of correct colors in the guess.
     """
-    counter = 0
+    counter_wrong_position = 0
     for i in range(4):
         for j in range(4):
             if guess[j] == random_color_sequence[i]:
                 counter_wrong_position += 1
     
-    return counter_wrong_position
+    pion_blanc = ["aucun pion blanc", "un pion blanc", "deux pions blancs", "trois pions blancs", "quatre pions blancs"][counter_wrong_position]
+
+    return pion_blanc
 
 def correct_colors_in_correct_position(random_color_sequence, guess):
     """
     Count the number of correct colors in the correct position.
     """
-    counter = 0
+    counter_correct_position = 0
     for i in range(4):
         if guess[i] == random_color_sequence[i]:
             counter_correct_position += 1
     
-    return counter_correct_position
+    pion_noir = ["aucun pion noir", "un pion noir", "deux pions noirs", "trois pions noirs", "quatre pions noirs"][counter_correct_position]
+    
+    return pion_noir
 
 random_color_sequence = generate_random_color_sequence()
-print("Random Color Sequence:", random_color_sequence)
-print('Le nombre de bonnes couleurs sont (mais pas bien placé /!\ bug pout l\'instant):', correct_colors_wrong_position(random_color_sequence, ["R", "G", "B", "Y"]))
-print('Le nombre de bonnes couleurs et bien placé:', correct_colors_in_correct_position(random_color_sequence, ["R", "G", "B", "Y"]))
+print("Votre tentative :", random_color_sequence)
+print('(/!\ bug pout l\'instant):', correct_colors_wrong_position(random_color_sequence, ["R", "G", "B", "Y"]))
+print(correct_colors_in_correct_position(random_color_sequence, ["R", "G", "B", "Y"]))
