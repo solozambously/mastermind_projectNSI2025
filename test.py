c7f2731c7e85c70d8455c8f534fe07c3d6cb88e4
SoloZambously Developpement <zambouslysolo@gmail.com>
2025-04-07 11:00:32 +0200

    add gitignore and test file

--- test.py
+++ test.py
@@ -1,8 +1,9 @@
-test = ['b', 'g']
+from main import *
 
-test2 = ['a', 'b', 'c', 'd']
 
-if test2[1] not in test:
-    print('b not in test')
-else:
-    print('b in test')
+random_color_sequence = generate_random_color_sequence()
+correct_colors_in_correct_position = correct_colors_in_correct_position(random_color_sequence, ["R", "G", "B", "Y"])
+correct_colors_wrong_position = correct_colors_wrong_position(random_color_sequence, ["R", "G", "B", "Y"])
+print("La combinaison secrète est :", random_color_sequence)
+print("Votre tentative :            ['R', 'G', 'B', 'Y']")
+print(define_numbers_pions(correct_colors_wrong_position, correct_colors_in_correct_position))
