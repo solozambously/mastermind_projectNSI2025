da34d0ec0984bf8b4df8a6b2a5425ef588a48d9d
SoloZambously Developpement <zambouslysolo@gmail.com>
2025-04-07 11:45:21 +0200

    feat: Guess Color Sequence

---
+++ test.py
@@ -0,0 +1,9 @@
+from functions import *
+
+
+random_color_sequence = generate_random_color_sequence()
+correct_colors_in_correct_position = correct_colors_in_correct_position(random_color_sequence, ["R", "G", "B", "Y"])
+correct_colors_wrong_position = correct_colors_wrong_position(random_color_sequence, ["R", "G", "B", "Y"])
+print("La combinaison secrète est :", random_color_sequence)
+print("Votre tentative :            ['R', 'G', 'B', 'Y']")
+print(define_numbers_pions(correct_colors_wrong_position, correct_colors_in_correct_position))
